c389dd6b98765348aef2934605c47994fb94c6bc
wormchenli <wormchenli@gmail.com>
2025-03-30 22:01:51 -0600

    fixed lint error

--- src/app/api/folder/route.ts
+++ src/app/api/folder/route.ts
@@ -2,8 +2,7 @@ import "server-only";
 
 import { FolderController } from "@/controllers/folderController";
 import { NextResponse } from "next/server";
-
-export async function GET(request: Request) {
+export async function GET() {
     const folders = await FolderController.getAllFolders();
     return NextResponse.json(folders);
 }
